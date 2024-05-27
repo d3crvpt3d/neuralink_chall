be223ad987092a534aa83946399b15020b28ea37
D3crvpt3d <st4r.sprayer@gmail.com>
2024-05-27 20:10:06 +0200

    added tests to encode

--- encode/src/main.rs
+++ encode/src/main.rs
@@ -1,22 +1,58 @@
-mod create_table;
-mod check_arguments;
-mod huffman;
-
-use check_arguments::arg_check;
-use create_table::table;
+use rustfft::{num_complex::Complex, FftPlanner};
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use bincode::serialize;
+use huffman_compression::BitWriter;
+use huffman_compression::codecs::DefaultEncoder;
+use huffman_compression::tree::TreeBuilder;
+use huffman_compression::codecs::Encoder;
 
 fn main() {
     
     let args: Vec<String> = std::env::args().collect();
 
-    if !arg_check::has_right_arguments(&args){
+    if args.len() != 3{
+        eprintln!("Usage: <input file> <output file> (args.len: {})", args.len());
         return;
     }
 
-    let lut = table::construct_table(
-        &table::read_file_to_byte_vec(&args[1]).unwrap()
-    );
+    let samples: Vec<i16> = hound::WavReader::open(&args[1]).unwrap().samples::<i16>().map(|x| x.unwrap()).collect();
 
+    let mut buffer = samples;
 
+    let mut planner = FftPlanner::new();
 
+    let fft = planner.plan_fft_forward(buffer.len());
+
+    fft.process(&mut buffer);
+
+    compress_and_write_huffman(buffer, Path::new(&args[2])).unwrap();
+
+}
+
+fn compress_and_write_huffman(data: Vec<i16>, output_path: &Path) -> Result<(), Box<dyn Error>> {
+    // Serialize the data to bytes
+    let serialized_data = serialize(&data)?;
+
+    // Build Huffman tree
+    let mut tree_builder = TreeBuilder::new();
+    tree_builder.extend_from_slice(&serialized_data);
+    let tree = tree_builder.build_tree();
+
+    // Encode the data
+    let mut encoder = DefaultEncoder::from_tree(tree);
+    let mut compressed_data = Vec::new();
+    {
+        let mut bit_writer = BitWriter::new(&mut compressed_data);
+        encoder.encode(&mut bit_writer, &serialized_data)?;
+        bit_writer.finish()?;
+    }
+
+    // Write compressed data to file
+    let mut file = File::create(output_path)?;
+    file.write_all(&compressed_data)?;
+
+    Ok(())
 }
