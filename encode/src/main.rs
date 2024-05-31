c0682dcd113cfa0fa64e92c4f6a81fe0fa2045b4
D3crvpt3d <st4r.sprayer@gmail.com>
2024-05-31 17:30:47 +0200

    added padding and string to bytes

--- encode/src/main.rs
+++ encode/src/main.rs
@@ -1,28 +1,31 @@
-use std::io::BufReader;
+use std::{fs::File, io::{BufReader, Write}};
+use num_bigint::BigUint;
 
-fn main() -> Result<_, Error>{
+//ALL IS LITTLE ENDIAN
+fn main(){
 
 	let args: Vec<String> = get_args();
 
-	let mut file_iterator: BufReader = BufReader::new(File::open(args.get(1))?).except("cant read File \"{}\"", args.get(1));
+	let mut file_iterator: BufReader<File> = BufReader::new(File::open(args.get(1).unwrap()).expect("cant read File"));
 
-	let mut out_buff: String = String::new();
-
-	encode(&mut File, &mut String);
-
-	save(out_buff);
+	save(encode(&mut file_iterator), args);
 
 }
 
 
-fn save(buf: String){
-	File::write(args.get(2), buf.to_str()).except("cant write file {}", args.get(2));
+
+fn save(buf: Vec<u8>, args: Vec<String>){
+
+	let mut file = File::create(args.get(2).unwrap()).expect("cant create Output File");
+
+	file.write_all(&buf).expect("cant write to file");
 }
+
 
 
 fn get_args() -> Vec<String>{
 
-	let args = std::env::args().collect();
+	let args: Vec<String> = std::env::args().collect();
 
 	if args.len() != 3 {
 		panic!("Args length should be 3 not {}", args.len());
@@ -32,6 +35,46 @@ fn get_args() -> Vec<String>{
 }
 
 
-fn encode(){
-	s
+//sequentially encodes byte Vec with arithmetic encoding
+fn encode(f: &mut BufReader<File>) -> Vec<u8>{
+	
+	let mut out_vec: Vec<u8> = Vec::new();
+
+	let mut numerator: BigUint = BigUint::ZERO;
+	let mut denominator: BigUint = BigUint::new(vec![1]);
+
+	//TODO
+
+
+
+	
+	let mut header: Vec<u8> = create_arith_header(1, 1);
+
+	header.append(&mut out_vec);
+
+	header
+}
+
+
+// "00101010" -> 00101010u8
+fn string_to_byte_vec(string: &mut String) -> Vec<u8>{
+
+	//pad with zeros
+	while string.len() % 8 != 0 {
+			string.push('0');
+	}
+	
+	string.as_bytes().chunks(8)
+		.map(|chunk| std::str::from_utf8(chunk).unwrap())
+		.map(|chunk_str| u8::from_str_radix(chunk_str, 2).unwrap())
+		.collect()
+
+}
+
+
+//returns the header for arithmetic encoding in bits (header_size = 16byte)
+fn create_arith_header(numerator_length: u64, denominator_length: u64) -> Vec<u8>{
+
+	[numerator_length.to_le_bytes(), denominator_length.to_le_bytes()].concat()
+
 }
